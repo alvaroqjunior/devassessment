156c2e5a272c737b4caf340b8d2f92dd29b703da
Alvaro Quevedo Junior <alvaroquevedo@live.com>
2020-09-15 15:19:43 -0300

    initial commit

---
+++ Graph/Graph.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Graph
+{
+    public interface IGraph<T>
+    {
+        IObservable<IEnumerable<T>> RoutesBetween(T source, T target);
+    }
+
+    public class Graph<T> : IGraph<T>
+    {
+        public Graph(IEnumerable<ILink<T>> links)
+        {
+
+        }
+
+        public IObservable<IEnumerable<T>> RoutesBetween(T source, T target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
