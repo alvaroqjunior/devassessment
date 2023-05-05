050563ea3ec89d2051200fdde315be80f03fcc46
TyperIgor <igor_purosso@hotmail.com>
2023-05-05 10:27:55 -0300

    Update challenge solution

--- Graph/Graph.cs
+++ Graph/Graph.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
 
 namespace Graph
 {
@@ -10,14 +13,87 @@ namespace Graph
 
     public class Graph<T> : IGraph<T>
     {
+        Dictionary<T, List<T>> graphConnectionsList = new Dictionary<T, List<T>>();
+
         public Graph(IEnumerable<ILink<T>> links)
         {
+            // Cria um dicionário para armazenar os vertices dos grafos e suas possives arestas existentes
+            graphConnectionsList = new Dictionary<T, List<T>>();
 
+            foreach (var link in links)
+            {
+                if (!graphConnectionsList.ContainsKey(link.Source))
+                    graphConnectionsList.Add(link.Source, new List<T>());
+
+                graphConnectionsList[link.Source].Add(link.Target);
+            }
         }
 
-        public IObservable<IEnumerable<T>> RoutesBetween(T source, T target)
+        public IObservable<IEnumerable<T>> RoutesBetween(T source, T destination)
         {
-            throw new NotImplementedException();
+            return Observable.Create<IEnumerable<T>>(observer =>
+            {
+                // Cria uma lista para armazenar os vertices visitados e outra lista para o caminho realizado 
+                var visited = new HashSet<T>();
+                var path = new List<T>();
+
+                // Chama o método que encontra as rotas possíveis, passando o observador e os valores iniciais
+                FindAllPossiblesRoutes(source, destination, visited, path, observer);
+
+                // Emite o sinal de conclusão
+                observer.OnCompleted();
+
+                // Retorna um Action que será chamado quando o Observable for descartado
+                return () => { };
+            });
         }
+
+        // Método recursivo que encontra as rotas possíveis entre um ponto de origem e um de destino
+        private void FindAllPossiblesRoutes(T current, T target, HashSet<T> visited, List<T> path, IObserver<IEnumerable<T>> observer)
+        {
+            
+            
+            visited.Add(current);
+            path.Add(current);
+
+            // Se o nó atual é o nó de destino, emite uma nova lista com o caminho percorrido
+            if (current.Equals(target))
+            {
+                observer.OnNext(new List<T>(path));
+            }
+            else if (graphConnectionsList.ContainsKey(current))
+            {
+                // Senão, para cada vizinho do nó atual, se ele ainda não foi visitado, chama o método novamente passando-o como nó atual
+                foreach (var neighbor in graphConnectionsList[current])
+                {
+                    if (!visited.Contains(neighbor))
+                        FindAllPossiblesRoutes(neighbor, target, visited, path, observer);
+                }
+            }
+
+            // Remove o nó atual das listas de visitados e de caminho antes de retornar
+            visited.Remove(current);
+            path.RemoveAt(path.Count - 1);
+        }
+
+        //private bool HasConnectionsOnGraph(int i, string destination)
+        //{
+        //    if (_connections.Contains(array[i].Source) || _connections.Contains(array[i].Target))
+        //        return true;        
+
+        //    return false;
+        //}
     }
 }
+//var j = 0;
+
+//_connections.Add(new { array[j].Source, array[j].Target });
+
+//for (int i = 0; i < array.Length; i++) // O (n)
+//{
+//    if (HasConnectionsOnGraph(i, destination.ToString()))
+//        _connections.Add(new { array[i].Source, array[i].Target });
+
+//    if (array[i].Source == destination.ToString() || array[i].Target == destination.ToString())
+//        continue;
+//}
--- SubtitleTimeshift/Shifter.cs
+++ SubtitleTimeshift/Shifter.cs
@@ -9,7 +9,45 @@ namespace SubtitleTimeshift
     {
         async static public Task Shift(Stream input, Stream output, TimeSpan timeSpan, Encoding encoding, int bufferSize = 1024, bool leaveOpen = false)
         {
-            throw new NotImplementedException();
+            const string subtitleDivider = " --> ";
+            try
+            {
+                using (var inputReaderData = new StreamReader(input, encoding, true))
+                using (var outputData = new StreamWriter(output, encoding))
+                {
+                    string line;
+                    while(!inputReaderData.EndOfStream)
+                    {
+                        line = inputReaderData.ReadLine();
+
+                        if (!line.Contains(subtitleDivider))
+                        {
+                            await outputData.WriteLineAsync(line);
+                            continue;
+                        }
+
+                        var lineParsed = ShifterSubtitlesOnStream(line, timeSpan);
+                        await outputData.WriteLineAsync(lineParsed);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error message: {e.Message}");
+                throw;
+            }
+
+            string ShifterSubtitlesOnStream(string line, TimeSpan timespan)
+            {
+                var timesSubtitlesSplited = line.Split(new[] { subtitleDivider }, StringSplitOptions.None);
+                for (int i = 0; i < timesSubtitlesSplited.Length; i++)
+                {
+                    var parsedDataSubtitle = TimeSpan.Parse(timesSubtitlesSplited[i]);
+                    timesSubtitlesSplited[i] = parsedDataSubtitle.Add(timespan).ToString(@"hh\:mm\:ss\.fff");
+                }
+
+                return timesSubtitlesSplited[0] + subtitleDivider + timesSubtitlesSplited[1].ToString();
+            }
         }
     }
 }
